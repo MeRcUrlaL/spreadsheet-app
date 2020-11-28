cdd3f3a6eebc5a2c55db557ea7047cd904f08011
Overlord <mercurial.yandex@mail.ru>
2020-11-28 20:10:07 +0300

    configured webpack, added and configured eslint

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,71 @@
 const path = require('path')
-const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HTMLWebpackPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
+const ASSET_PATH = process.env.ASSET_PATH || './'
+
+const isProd = process.env.NODE_ENV === 'production'
+const isDev = !isProd
+
+const filename = (ext) => (isDev ? `bundle.${ext}` : `bundle.[hash].${ext}`)
+
+const jsLoaders = () => {
+  const loaders = [
+    {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env'],
+      },
+    },
+  ]
+
+  if (isDev) {
+    loaders.push('eslint-loader')
+  }
+
+  return loaders
+}
+
 module.exports = {
   context: path.resolve(__dirname, 'src'),
-  mode: "development",
-  entry: './index.js',
+  mode: 'development',
+  entry: ['@babel/polyfill', './index.js'],
   output: {
-    filename: 'bundle.[hash].js',
-    path: path.resolve(__dirname, 'dist')
+    filename: filename('js'),
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: ASSET_PATH,
   },
+  target: process.env.NODE_ENV === 'development' ? 'web' : 'browserslist',
   resolve: {
     extensions: ['.js'],
     alias: {
       '@': path.resolve(__dirname, 'src'),
-      '@core': path.resolve(__dirname, 'src/core')
-    }
+      '@core': path.resolve(__dirname, 'src/core'),
+    },
+  },
+  devtool: isDev ? 'source-map' : false,
+  devServer: {
+    port: 3000,
+    hot: isDev,
   },
   plugins: [
     new CleanWebpackPlugin(),
     new HTMLWebpackPlugin({
-      template: 'index.html'
+      template: 'index.html',
     }),
     new CopyPlugin({
       patterns: [
         {
           from: path.resolve(__dirname, 'src/favicon.ico'),
-          to: path.resolve(__dirname, 'dist')
+          to: path.resolve(__dirname, 'dist'),
         },
       ],
     }),
     new MiniCssExtractPlugin({
-      filename: 'bundle.[hash].css'
-    })
+      filename: filename('css'),
+    }),
   ],
   module: {
     rules: [
@@ -49,13 +80,8 @@ module.exports = {
       {
         test: /\.m?js$/,
         exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env']
-          }
-        }
+        use: jsLoaders(),
       },
-    ]
-  }
+    ],
+  },
 }
